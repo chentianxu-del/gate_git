63cdf154ecb0fb725c6115ec29c758162de7bd03
2020607281@qq.com <chentianxu@longshine.com>
2023-03-13 20:27:42 +0800

    第二次提交

--- gateway/src/main/java/com/ctx/controller/MadeController.java
+++ gateway/src/main/java/com/ctx/controller/MadeController.java
@@ -1,5 +1,13 @@
 package com.ctx.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.*;
+
 /**
  * @program: gateway
  * @ClassName MadeController
@@ -8,5 +16,12 @@ package com.ctx.controller;
  * @create: 2023-01-30 15:17
  * @Version 1.0
  **/
+@RequestMapping("/user")
+@RestController
 public class MadeController {
+
+    @PostMapping(value = "/12345", consumes= MediaType.APPLICATION_JSON_VALUE)
+    public String getUser(){
+        return "ceshi";
+    }
 }
